ba6c6ce714b3c006ed06ff27ede687b4d297ad21
Federico Tomassetti <f.tomassetti@gmail.com>
2014-07-20 12:35:29 +0100

    First working version

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject app "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [com.google.code.javaparser/javaparser "1.0.11"]])
+                 [com.google.code.javaparser/javaparser "1.0.12-SNAPSHOT"]
+                 [org.clojure/tools.cli "0.2.1"]]
+  :main app.core)
--- src/app/core.clj
+++ src/app/core.clj
@@ -1,22 +1,22 @@
-(ns app.core)
+(ns app.core
+  (:require [clojure.java.io :as io])
+  (:use [clojure.tools.cli :only (cli)])
+  (:gen-class :main true))
 
 (import japa.parser.JavaParser)
+(import japa.parser.ast.CompilationUnit)
 (import japa.parser.ast.body.ClassOrInterfaceDeclaration)
-(import  japa.parser.ast.body.ConstructorDeclaration)
+(import japa.parser.ast.body.ConstructorDeclaration)
 
-(defn parse [filename]
-  (parseFile (new java.io.File filename)))
+; ============================================
+; Misc
+; ============================================
 
-(defn parseFile [file]
-  (try
-    (JavaParser/parse file)
-    (catch Exception e nil)))
+(def not-nil? (complement nil?))
 
-(defn parseAllFiles [files]
-  (map (fn [f] [f (parseFile f)]) files))
-
-(defn parseAllDir [dirname]
-  (parseAllFiles (java-files dirname)))
+; ============================================
+; Files
+; ============================================
 
 (defn java-file? [f]
   (and (.isFile f)
@@ -26,6 +26,24 @@
   (let [d (java.io.File. dirname)]
     (filter java-file? (file-seq d))))
 
+; ============================================
+; Parsing
+; ============================================
+
+(defn parseFile [file]
+  (try
+    (JavaParser/parse file)
+    (catch Exception e nil)))
+
+(defn parse [filename]
+  (parseFile (new java.io.File filename)))
+
+(defn parseAllFiles [files]
+  (map (fn [f] {:file f :cu (parseFile f)}) files))
+
+(defn parseAllDir [dirname]
+  (parseAllFiles (java-files dirname)))
+
 (defn parseDir [dirname]
   (let [d (java.io.File. dirname)]
     (doseq [f (.listFiles d)]
@@ -34,24 +52,47 @@
         (print "- "))
       (println (.getName f)))))
 
+; ============================================
+; Model
+; ============================================
+
 (defn isClass? [n]
   (and
     (instance? ClassOrInterfaceDeclaration n)
     (not (.isInterface n))))
 
-; Consider internal classes
+(defn packageName [n]
+  (if (instance? CompilationUnit n)
+    (let
+      [p (.getPackage n)]
+      (if (nil? p)
+        ""
+        (.toString (.getName p))))
+    (try
+      (let [pn (.getParentNode n)]
+        (if (nil? pn)
+          ""
+          (packageName pn)))
+    (catch Exception e (str "STR PN " (nil? (.getParentNode n)) e) ))))
+
+(defn classQname [cl]
+  (let
+      [pn (packageName cl),
+      cn (.getName cl)]
+    (if (.isEmpty pn)
+      cn
+      (str pn "." cn))))
+
+; TODO Consider internal classes
 (defn getClasses [cu]
   (filter isClass? (.getTypes cu)))
 
-(def not-nil? (complement nil?))
-
-(def filename "/home/federico/repos/javaparser/src/main/java/japa/parser/Position.java")
-(def dirname "/home/federico/repos/javaparser/src")
 ; Get tuples of [filename cu]
-(def cusTuples
+(defn cusTuples [dirname]
   (filter not-nil? (parseAllDir dirname)))
-(def cus
-  (map (fn [[_ cu]] cu) cusTuples))
+
+(defn cus [dirname]
+  (map (fn [cuTuple] (:cu cuTuple)) (cusTuples dirname)))
 
 (defn getConstructors [cl]
   (filter (fn [m] (instance? ConstructorDeclaration m)) (.getMembers cl)))
@@ -59,6 +100,81 @@
 (defn nConstructors [cl]
   (.size (getConstructors cl)))
 
+(defn getParameters [m]
+  (let [ps (.getParameters m)]
+    (if (nil? ps)
+      (java.util.ArrayList. )
+      ps)))
 
-(defn constructors [classDecl]
-  )
+; ============================================
+; ITEM 1
+; ============================================
+
+(defn printClassesWithManyConstructors [cus threshold]
+  (doseq [cu cus]
+    (doseq [cl (getClasses cu)]
+      (let [nc (nConstructors cl)]
+        (if (>= nc threshold)
+          (println (classQname cl) " : " nc)
+          nil)))))
+
+; ============================================
+; ITEM 2
+; ============================================
+
+(defn printConstructorsWithManyParameters [cus threshold]
+  (doseq [cu cus]
+    (doseq [cl (getClasses cu)]
+      (doseq [cs (getConstructors cl)]
+        (let [np (.size (getParameters cs))]
+          (if (>= np threshold)
+            (println (classQname cl) "." cs " : " np)
+            nil))))))
+
+; ============================================
+; CLI
+; ============================================
+
+(defn name2query [name]
+  (cond
+    (= "mc" name) printClassesWithManyConstructors
+    (= "mcp" name) printConstructorsWithManyParameters
+    :else nil))
+
+(defn threshold2number [th]
+  (if (number? th)
+    th
+    (try
+      (Integer/parseInt th)
+      (catch NumberFormatException e nil))))
+
+(defn run [opts]
+  (let [dirname (:dir opts)
+        th (threshold2number (:threshold opts))
+        query (name2query (:query opts))
+        cus (cus dirname)]
+    (do
+      (println "Considering" (.size cus) "Java files")
+      (query cus th))))
+
+(defn -main
+  "What I do"
+  [& args]
+  (let [[opts args banner]
+    (cli args
+      ["-h" "--help" "Show help" :flag true :default false]
+      ["-d" "--dir" "REQUIRED: Directory containing the code to check"]
+      ["-q" "--query" "REQUIRED: Query to perform: mc=many constructors, mcp=many constructor parameters"]
+      ["-t" "--threshold" "Threshold to be used in the query" :default 0]
+      )]
+    (when (:help opts)
+      (println banner)
+      (System/exit 0))
+    (if
+      (and
+        (:dir opts)
+        (:query opts)
+        (name2query (:query opts))
+        (threshold2number (:threshold opts) ))
+      (run opts)
+      (println banner))))
