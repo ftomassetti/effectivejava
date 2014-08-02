f672e506960c84802bd41bd4ae85545cdb72fcf2
Federico Tomassetti <f.tomassetti@gmail.com>
2014-08-02 09:25:57 +0100

    Preparing for new release

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject effectivejava "0.1.1-SNAPSHOT"
-  :description "A tool for running queries on your Java codebase"
+(defproject effectivejava "0.1.2-SNAPSHOT"
+  :description "A Java linter and a tool for running queries on your Java codebase"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.google.code.javaparser/javaparser "1.0.12-SNAPSHOT"]
                  [org.clojure/tools.cli "0.3.1"]
