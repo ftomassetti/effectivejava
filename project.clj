b2f1e119cb8cee54940ce82dddd6419400568d08
Federico Tomassetti <federico@tomassetti.me>
2015-04-04 09:12:54 +0100

    Switching to JavaParser2

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject effectivejava "0.1.2-SNAPSHOT"
   :description "A Java linter and a tool for running queries on your Java codebase"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.google.code.javaparser/javaparser "1.0.12-SNAPSHOT"]
+                 [com.github.javaparser/javaparser-core "2.0.0"]
                  [org.clojure/tools.cli "0.3.1"]
                  [instaparse "1.3.3"]]
   :resource-paths ["test-resources"]
