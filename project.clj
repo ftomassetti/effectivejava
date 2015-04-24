f2affc6a00fdcc3741312e621e5075fb96f90b5f
Federico Tomassetti <ftomassetti@groupon.com>
2015-04-24 17:33:22 +0100

    move to instaparse 1.3.6

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.google.code.javaparser/javaparser "1.0.11"]
                  [org.clojure/tools.cli "0.3.1"]
-                 [instaparse "1.3.3"]]
+                 [instaparse "1.3.6"]]
   :resource-paths ["test-resources"]
   :main app.core)
