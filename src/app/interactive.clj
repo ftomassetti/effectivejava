2026dda80a926ef46b98e623b292b1976413a9ee
David Ortiz <z.david.ortiz@gmail.com>
2015-05-08 11:44:31 +0200

    Added many-constructor-params op to interactive mode

--- src/app/interactive.clj
+++ src/app/interactive.clj
@@ -13,16 +13,17 @@
 (def command-parser
   (insta/parser
    (str
-    "<COMMAND> = HELP | EXIT | LOAD | LIST | MC | F        \n"
-    "HELP = 'help' | 'h'                                   \n"
-    "EXIT = 'exit' | 'quit' | 'q'                          \n"
-    "LOAD = 'load' <WS> STR                                \n"
-    "LIST = 'list'                                         \n"
-    "MC  = ('mc'|'many-constructors') <WS> 'th' <WS> NUM   \n"
-    "F = ('f'|'finalizers')                                \n"
-    "WS  = #'[\t ]+'                                       \n"
-    "NUM = #'[0-9]+'                                       \n"
-    "STR = #'\"[^\"]*\"'                                   \n")))
+    "<COMMAND> = HELP | EXIT | LOAD | LIST | MC | MCP | F        \n"
+    "HELP = 'help' | 'h'                                         \n"
+    "EXIT = 'exit' | 'quit' | 'q'                                \n"
+    "LOAD = 'load' <WS> STR                                      \n"
+    "LIST = 'list'                                               \n"
+    "MC  = ('mc'|'many-constructors') <WS> 'th' <WS> NUM         \n"
+    "MCP = ('mcp'|'many-costructor-params') <WS> 'th' <WS> NUM   \n"
+    "F = ('f'|'finalizers')                                      \n"
+    "WS  = #'[\t ]+'                                             \n"
+    "NUM = #'[0-9]+'                                             \n"
+    "STR = #'\"[^\"]*\"'                                         \n")))
 
 (declare interactive)
 
@@ -41,12 +42,13 @@
     (interactive state)))
 
 (defn- help [state]
-  (println "h/help                      : print this help message")
-  (println "q/quit/exit                 : close the shell")
-  (println "list                        : list classes loaded")
-  (println "load DIR                    : load classes from DIR")
-  (println "mc/many-constructors th NUM : list classes with NUM or more constructors")
-  (println "f/finalizers                : list classes that use finalizers")
+  (println "h/help                             : print this help message")
+  (println "q/quit/exit                        : close the shell")
+  (println "list                               : list classes loaded")
+  (println "load DIR                           : load classes from DIR")
+  (println "mc/many-constructors th NUM        : list classes with NUM or more constructors")
+  (println "mcp/many-constructor-params th NUM : list constructors with NUM or more parameters")
+  (println "f/finalizers                       : list classes that use finalizers")
   (interactive state))
 
 (defn- load-classes [ast]
@@ -59,6 +61,10 @@
 
 (defn- mc-operation [state threshold]
   (printOperation classesWithManyConstructorsOp (:cus state) threshold)
+  (interactive state))
+
+(defn- mcp-operation [state threshold]
+  (printOperation constructorsWithManyParametersOp (:cus state) threshold)
   (interactive state))
 
 (defn- f-operation [state]
@@ -81,6 +87,8 @@
           :LOAD (load-classes ast)
           :MC (let [threshold (read-string (last (last (first ast))))]
                 (mc-operation state threshold))
+          :MCP (let [threshold (read-string (last (last (first ast))))]
+                 (mcp-operation state threshold))
           :F (f-operation state)
           (println "Command not implemented: " command))))))
 
