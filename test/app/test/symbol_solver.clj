a8c923d6af3bf6ec41f4493445402d46e719532a
Federico Tomassetti <ftomassetti@groupon.com>
2015-05-11 13:53:51 +0100

    fix misplaced parentheis in test/symbol_solver

--- test/app/test/symbol_solver.clj
+++ test/app/test/symbol_solver.clj
@@ -11,9 +11,9 @@
   (first (filter (fn [c] (= name (.getName c))) sampleClasses)))
 
 (deftest testPreceedingChildren
-  (is (= '(1 2 3 4)) (preceedingChildren [1 2 3 4 5] 5))
-  (is (= '(1 2)) (preceedingChildren [1 2 3 4 5] 3))
-  (is (= '()) (preceedingChildren [1 2 3 4 5] 1)))
+  (is (= '(1 2 3 4) (preceedingChildren [1 2 3 4 5] 5)))
+  (is (= '(1 2) (preceedingChildren [1 2 3 4 5] 3)))
+  (is (= '() (preceedingChildren [1 2 3 4 5] 1))))
 
 (deftest testSolveNameInVariableDeclarator
   (let [aClassResolvingToLocalVar (sampleClass "AClassResolvingToLocalVar")
