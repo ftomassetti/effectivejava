ea392a6937ba0453d8fcbe617c70f2018e3d362f
Federico Tomassetti <federico@tomassetti.me>
2015-05-12 21:21:08 +0100

    test cli-opts

--- test/app/test/cli.clj
+++ test/app/test/cli.clj
@@ -1,4 +1,5 @@
 (ns app.test.cli
+  (:require [clojure.tools.cli :refer [parse-opts]])
   (:use [app.cli])
   (:use [app.core])
   (:use [app.model.protocols])
@@ -10,6 +11,77 @@
 (def javaparserCus (cus "test-resources/sample-codebases/javaparser/"))
 
 (deftest issue43
+  "mcp query fails when no threshold is specified"
   (let [op (:mcp operations)]
     (with-redefs [println (fn [_] 1)]
       (printOperation op javaparserCus 0))))
+
+(deftest cli-opts-help
+  (let [args ["--help"]
+        res (parse-opts args cliOpts)]
+    (is (= [] (:arguments res)))
+    (is (= false (:linter (:options res))))
+    (is (= false (:interactive (:options res))))
+    (is (= true (:help (:options res))))
+    (is (= nil (:errors res)))))
+
+(deftest cli-opts-interactive-short
+  (let [args ["-i"]
+        res (parse-opts args cliOpts)]
+    (is (= [] (:arguments res)))
+    (is (= false (:linter (:options res))))
+    (is (= true (:interactive (:options res))))
+    (is (= false (:help (:options res))))
+    (is (= nil (:errors res)))))
+
+(deftest cli-opts-interactive-long
+  (let [args ["--interactive"]
+        res (parse-opts args cliOpts)]
+    (is (= [] (:arguments res)))
+    (is (= false (:linter (:options res))))
+    (is (= true (:interactive (:options res))))
+    (is (= false (:help (:options res))))
+    (is (= nil (:errors res)))))
+
+(deftest cli-opts-linter-short
+  (let [args ["-l"]
+        res (parse-opts args cliOpts)]
+    (is (= [] (:arguments res)))
+    (is (= true (:linter (:options res))))
+    (is (= false (:interactive (:options res))))
+    (is (= false (:help (:options res))))
+    (is (= nil (:errors res)))))
+
+(deftest cli-opts-linter-long
+  (let [args ["--linter"]
+        res (parse-opts args cliOpts)]
+    (is (= [] (:arguments res)))
+    (is (= true (:linter (:options res))))
+    (is (= false (:interactive (:options res))))
+    (is (= false (:help (:options res))))
+    (is (= nil (:errors res)))))
+
+(deftest cli-opts-unknown
+  (let [args ["--foo"]
+        res (parse-opts args cliOpts)]
+    (is (:errors res))))
+
+(deftest cli-opts-dir-short
+  (let [args ["-d" "FOO"]
+        res (parse-opts args cliOpts)]
+    (is (= [] (:arguments res)))
+    (is (= false (:linter (:options res))))
+    (is (= false (:interactive (:options res))))
+    (is (= false (:help (:options res))))
+    (is (= "FOO" (:dir (:options res))))
+    (is (= nil (:errors res)))))
+
+(deftest cli-opts-dir-long
+  (let [args ["--dir" "FOO"]
+        res (parse-opts args cliOpts)]
+    (is (= [] (:arguments res)))
+    (is (= false (:linter (:options res))))
+    (is (= false (:interactive (:options res))))
+    (is (= false (:help (:options res))))
+    (is (= "FOO" (:dir (:options res))))
+    (is (= nil (:errors res)))))
