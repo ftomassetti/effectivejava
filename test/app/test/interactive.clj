8a321cee5441dfc18b233fec2daa28c4dc9c2189
David Ortiz <z.david.ortiz@gmail.com>
2015-05-15 20:45:22 +0200

    Add test for show-help in interactive mode

--- test/app/test/interactive.clj
+++ test/app/test/interactive.clj
@@ -12,6 +12,16 @@
                (verify-call-times-for println 1)
                (verify-first-call-args-for println "Exit...")))))
 
+(deftest shows-help
+  (let [command-sequence ["help" "quit"]
+        input-string (clojure.string/join "\r" command-sequence)]
+    (with-in-str
+      input-string
+      (mocking [println print flush]
+               (interactive [])
+               (verify-call-times-for println 2)
+               (verify-first-call-args-for println help-message)))))
+
 (deftest list-shows-error-if-no-classes-loaded
   (let [command-sequence ["list" "quit"]
         input-string (clojure.string/join "\r" command-sequence)]
